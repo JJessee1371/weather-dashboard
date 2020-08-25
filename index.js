d8ec9420e7f552937feeca8b24a76e31be2eb9bc
Jon Jessee <tjessee7624@gmail.com>
2020-08-24 22:17:10 -0600

    Started work on second ajax request and setting local storage to make a search history.

--- index.js
+++ index.js
@@ -15,20 +15,22 @@ const day4 = $('#day1');
 const day5 = $('#day1');
 
 console.log('Sanity Check');
-//Load local storage items
-for (i = 0; i < localStorage.length; i++) {
-    let savedCity = localStorage.getItem(localStorage.key(i));
-    let listItem = $('<tr>');
-    table.append(listItem.text(savedCity));
-}
-//Local Storage will clear out at the end of each day
+//Load local storage items into search history
+// for (i = 0; i < localStorage.length; i++) {
+//     let savedCity = localStorage.getItem(localStorage.key(i));
+//     let listItem = $('<tr>');
+//     table.append(listItem.text(savedCity));
+// }
+
+
+//Local Storage will clear at the end of each day
 let now = moment();
 let currentHour = now.hour();
-if (currentHour === 0) {localStorage.clear()};
+if (currentHour === 0) { localStorage.clear() };
 
 //Key variable ensures a new key is created for each save to local storage
 let key = 1;
-searchBtn.on('click', function() {
+searchBtn.on('click', function () {
     let cityName = userInput.val().trim();
     let currentDay = moment().format("MM/DD/YYYY");
 
@@ -37,25 +39,48 @@ searchBtn.on('click', function() {
     $.ajax({
         url: queryURL,
         method: 'GET'
-    })
-        //Returns response data from open weather
-        .then(function(response) {
-            console.log(response);
+    }).then(function (response) {
+        console.log(response);
+        //Saves city searches to the local storage and displays
+        localStorage.setItem('city' + key, cityName);
+        key++;
+        //Empty the previously set items in the card
 
-            //Saves city searches to the local storage and displays
-            localStorage.setItem('city' + key, cityName);
-            key++;
-            
-            temp.empty();
-            //Current days forecast
-            city.text('Todays weather in: ' + cityName + ' ' + currentDay);
-            //Convert Kelvin temp to Farenheit
-            let k = response.main.temp;
-            let f = Math.floor(1.8 * (k - 273) + 32);
-            temp.text('Temperature:' + ' '  + f + ' ' + '°F');
-            humidity.text('Humidity:' + ' ' + response.main.humidity + '%');
-            wind.text('Wind speed:' + ' ' + response.wind.speed + 'mph');
-            // uv.text('UV Index:' + ' ' + )
-        })
+        //Current days forecast
+        city.text('Todays weather in: ' + cityName + ' ' + currentDay);
+        //Convert Kelvin temp to Farenheit
+        let k = response.main.temp;
+        let f = Math.floor(1.8 * (k - 273) + 32);
+        temp.text('Temperature:' + ' ' + f + ' ' + '°F');
+        humidity.text('Humidity:' + ' ' + response.main.humidity + '%');
+        wind.text('Wind speed:' + ' ' + response.wind.speed + 'mph');
+        // uv.text('UV Index:' + ' ' + )
+
+        let savedCity = localStorage.getItem('city' + (key - 1));
+        console.log('city' + key);
+        let listItem = $('<tr>');
+        table.append(listItem.text(savedCity));
+    });
+
+    // api.openweathermap.org/data/2.5/forecast?q={city name}&appid={your api key}
+    let queryURL2 = 'https://api.openweathermap.org/data/2.5/forecast?q=' + cityName + '&appid=09009915e3fc252d07db5e780defa8fe';
+
+    $.ajax({
+        url: queryURL2,
+        method: 'GET'
+    }).then(function (response) {
+        console.log(response);
+        let day1Date = $('<p>').text(moment().add(1, 'days').format("MM/DD/YYYY"));
+        let day1k = response.main.temp;
+        let day1f = $('<p>').text(Math.floor(1.8 * (k - 273) + 32));
+        let day1Humidity = $('<p>').text(response.main.humidity);
+        day1.append(date, f, humidity);
+        
+
+    // console.log(moment().add(1, 'days').format("MM/DD/YYYY"));
+    // console.log(moment().add(2, 'days').format("MM/DD/YYYY"));
+
+    })
+
 
 })
