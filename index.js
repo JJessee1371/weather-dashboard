daa95b9507d849a2cbe5bdf407b3c5602d93e21b
Jon Jessee <tjessee7624@gmail.com>
2020-08-24 18:10:20 -0600

    Included jQuery CDN and declared JS variables.

---
+++ index.js
@@ -0,0 +1,9 @@
+//Variable declaration
+const day1 = $('#day1');
+const day2 = $('#day1');
+const day3 = $('#day1');
+const day4 = $('#day1');
+const day5 = $('#day1');
+const search = $('#search-history');
+const searchBtn = $('searchBtn');
+const today = $('card-title');
